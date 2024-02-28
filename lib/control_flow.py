ef04a128f901ac9c805dc50afd521237bc3d7db3
EStherMmutisya <starjacob994@gmail.com>
2024-02-28 22:40:57 +0300

    completed assignment

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,56 @@
 #!/usr/bin/env python3
 
+
 def admin_login(username, password):
+    if (username.upper() == "ADMIN" and password == "12345"):
+        return "Access granted"
+    else:
+        return "Access denied"
+
     # your code here
     pass
 
 def hows_the_weather(temperature):
-    # your code here
+    if temperature < 40:
+        return "It's brisk out there!"
+
+    elif 40 <= temperature <= 65:
+        return "It's a little chilly out there!"
+
+    elif temperature > 85:
+        return "It's too dang hot out there!"
+
+    else:
+        return "It's perfect out there!"
+
+
     pass
 
 def fizzbuzz(num):
-    # your code here
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+
+    elif num % 3 == 0:
+        return "Fizz"
+
+    elif num % 5 == 0:
+        return "Buzz"
+
+    else:
+        return num
+
+    
     pass
 
 def calculator(operation, num1, num2):
+    dict_map = {
+        "+": num1 + num2,
+        "-": num1 - num2,
+        "*": num1 * num2,
+        "/": num1 / num2 if num2 != 0 else "Cannot divide by zero"
+    }
+    result = dict_map.get(operation, "Invalid operation!")
+    print(result)
+    return result
     # your code here
     pass
